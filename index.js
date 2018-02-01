89523595ca3908c424831197d8c0aa9d45fb8f41
yellobanana <paulxuca@gmail.com>
2018-02-01 14:09:05 -0500

    Update to use pattern from aws-lambda-chrome

---
+++ index.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+// NOTE: This modifies the LD_LIBRARY_PATH value on lambda
+process.env.LD_LIBRARY_PATH = process.env.LD_LIBRARY_PATH + ':' + path.join(__dirname, 'dll');
+
+const vendorDir = path.join(__dirname, 'vendor');
+
+const pdfUnite = path.join(vendorDir, 'pdfunite');
+const pdfToPs = path.join(vendorDir, 'pdftops');
+const pdfSeperate = path.join(vendorDir, 'pdfseperate');
+
+module.exports = {
+    pdfUnite,
+    pdfToPs,
+    pdfSeperate
+};
